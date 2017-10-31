d21e7ee7be40b4dd70d72e10c0e6d90cd36376b9
SlavaKyrai <slavakyrai@gmail.com>
2017-10-31 22:31:16 +0200

    refactoring

--- shop_app/filters.py
+++ shop_app/filters.py
@@ -7,7 +7,6 @@ class AnimalFilter(django_filters.FilterSet):
     price__gt = django_filters.NumberFilter(name='price', lookup_expr='gt')
     price__lt = django_filters.NumberFilter(name='price', lookup_expr='lt')
 
-
     class Meta:
         model = Animal
         fields = ['type']
